139011828c52fdee1ccff45e5a3103a4d76eab0c
Nabeel Nazir <nabeeln796@yahoo.com>
2017-10-06 19:37:17 +0500

    changes

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -18,7 +18,10 @@ class GoalsController < ApplicationController
     @goal = Goal.new(goal_params)
     if @goal.save
       #puts "I AM CURL SYS CALL" + system("curl http://localhost:9292/faye -d message={'channel':'/messages/new', 'data':'hello'}'").to_s
-      `curl http://localhost:9292/faye -d 'message={"channel":"/messages/new", "data":"title: #{@goal.title}, email: #{@goal.email}"}'`
+      #`curl http://localhost:9292/faye -d 'message={"channel":"/messages/new", "data":#{JSON.parse @goal}"}'`
+      hash = []
+      hash.push(@goal.title,@goal.email, @goal.id )
+      `curl http://localhost:9292/faye -d 'message={"channel":"/messages/new", "data":"#{@goal.title.to_s+','+@goal.email.to_s+','+@goal.id.to_s}"}'`
       render json: @goal, status: :created, location: @goal
     else
       render json: @goal.errors, status: :unprocessable_entity
@@ -27,16 +30,22 @@ class GoalsController < ApplicationController
 
   # PATCH/PUT /goals/1
   def update
-    if @goal.update(goal_params)
+    @goal = Goal.find_by_id params[:goal_id]
+    if @goal and @goal.update(goal_params)
       render json: @goal
     else
-      render json: @goal.errors, status: :unprocessable_entity
+      render json: 'SOMETHING WENT WRON!', status: :unprocessable_entity
     end
   end
 
   # DELETE /goals/1
   def destroy
-    @goal.destroy
+    @goal = Goal.find_by_id params[:goal_id]
+    if @goal and @goal.destroy
+      render json: '', status: :created
+    else
+      render json: 'Goal Dont exist or something went wrong in deleting goal!', status: :unprocessable_entity
+    end
   end
 
   private
