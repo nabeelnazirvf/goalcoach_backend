59d1b88ba1979532a194612122e5b931a06e85e9
Nabeel Nazir <nabeeln796@yahoo.com>
2017-10-16 18:19:12 +0500

    service class implemented for goals controller and its business logic.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,69 +1,81 @@
 class GoalsController < ApplicationController
-  #before_action :set_goal, only: [:show, :update, :destroy]
+  #before_action :set_service_object, only: [:index, :show, :update, :destroy, :create, :goals_notifications]
 
   # GET /goals
   def index
-    user = User.find_by_id(params[:user_id])
-    if user
-      @goals = user.goals.order(created_at: :desc)
-      render json: @goals
+    @goal_service = GoalService.new(params[:user_id], params[:goal_id], 'index', nil)
+    command = @goal_service.call
+    if command.success?
+      render json: command.result.to_a
     else
-
+      render json: { error: command.errors }, status: :unprocessable_entity
     end
   end
 
   # GET /goals/1
   def show
-    render json: @goal
+    @goal_service = GoalService.new(params[:user_id], params[:goal_id], 'show', nil)
+    command = @goal_service.call
+    if command.success?
+      render json: command.result
+    else
+      render json: { error: command.errors }, status: :unprocessable_entity
+    end
   end
 
   # POST /goals
   def create
-    user = User.find_by_id(params[:user_id])
-    @goal = user.goals.new(goal_params)
-    if user && @goal.save
-      render json: @goal, status: :created, location: @goal
+    @goal_service = GoalService.new(params[:user_id], params[:goal_id], 'create', goal_params)
+    command = @goal_service.call
+    if command.success?
+      render json: command.result
     else
-      render json: @goal.errors, status: :unprocessable_entity
+      render json: { error: command.errors }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /goals/1
   def update
-    @goal = Goal.find_by_id_and_user_id(params[:goal_id], params[:user_id])
-    if @goal and @goal.update(goal_params)
-      render json: @goal
+    @goal_service = GoalService.new(params[:user_id], params[:goal_id], 'update', goal_params)
+    command = @goal_service.call
+    if command.success?
+      render json: command.result
     else
-      render json: 'SOMETHING WENT WRONG IN UPDATING GOAL!', status: :unprocessable_entity
+      render json: { error: command.errors }, status: :unprocessable_entity
     end
   end
 
   # DELETE /goals/1
   def destroy
-    @goal = Goal.find_by_id_and_user_id(params[:goal_id], params[:user_id])
-    if @goal and @goal.destroy
-      render json: '', status: :created
+    @goal_service = GoalService.new(params[:user_id], params[:goal_id], 'destroy', nil)
+    command = @goal_service.call
+    if command.success?
+      render json: command.result
     else
-      render json: 'Goal Dont exist or something went wrong in deleting goal!', status: :unprocessable_entity
+      render json: { error: command.errors }, status: :unprocessable_entity
     end
   end
 
   def goals_notifications
-    goals_notifications = []
-    Goal.includes(:user).order(created_at: :desc).each do |g|
-      goals_notifications.push({user_name: g.user.name, image_base: g.user.image_base, title: g.title, created_at: g.created_at})
+    @goal_service = GoalService.new(params[:user_id], params[:goal_id], 'goals_notifications', nil)
+    command = @goal_service.call
+    if command.success?
+      render json: command.result
+    else
+      render json: { error: command.errors }, status: :unprocessable_entity
     end
-    render json: goals_notifications
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_goal
-      @goal = Goal.find(params[:id])
-    end
+    # def set_service_object
+    #   @goal_service = GoalService.new(params[:user_id], params[:goal_id], nil, nil)
+    # end
 
     # Only allow a trusted parameter "white list" through.
     def goal_params
-      params.require(:goal).permit(:title, :email)
+      params.require(:goal).permit(:title, :email, :user_id)
     end
+
+    
+
 end
---
+++ app/services/goal_service.rb
@@ -0,0 +1,91 @@
+class GoalService
+  prepend SimpleCommand
+
+  def initialize(user_id, goal_id, method_type, goal_params)
+    @user_id = user_id
+    @goal_id = goal_id
+    @method_type = method_type
+    @goal_params = goal_params
+  end
+
+  def call
+    case @method_type
+      when 'index'
+        goals
+      when 'show'
+        show
+      when 'update'
+        update
+      when 'destroy'
+        destroy
+      when 'create'
+        create
+      when 'goals_notifications'
+        goals_notifications
+      else
+        goals
+      end
+  end
+
+  private
+
+  #attr_accessor :user_id, :goal_id
+
+  def user
+    u = User.find_by_id(@user_id)
+    return u if u
+    errors.add :user, 'User Not Found!'
+    nil
+  end
+
+  def goals
+    return user.goals if user
+    errors.add :user, 'User Not Found!'
+    nil
+  end
+
+  def create
+    if user
+      goal = user.goals.new(@goal_params)
+      return goal if goal.save
+      errors.add :goal, 'Unable to create goal!'
+      nil
+    else
+      errors.add :user, 'User Not Found!'
+      nil
+    end
+  end
+
+  def update
+    goal = Goal.find_by_id_and_user_id(@goal_id, @user_id)
+    if user && goal
+      return goal if goal.update(@goal_params)
+      errors.add :goal, 'Unable to update goal!'
+      nil
+    else
+      errors.add :user, 'User or Goal Not Found!'
+      nil
+    end
+  end
+
+  def destroy
+    goal = Goal.find_by_id_and_user_id(@goal_id, @user_id)
+    if user && goal
+      return goal if goal.destroy
+      errors.add :goal, 'Unable to delete goal!'
+      nil
+    else
+      errors.add :user, 'User or Goal Not Found!'
+      nil
+    end
+  end
+
+  def goals_notifications
+    goals_notifications = []
+    Goal.includes(:user).order(created_at: :desc).each do |g|
+      goals_notifications.push({user_name: g.user.name, image_base: g.user.image_base, title: g.title, created_at: g.created_at})
+    end
+    goals_notifications
+  end
+
+end
