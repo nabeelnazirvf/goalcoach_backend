0ff2f9ae1e97430d3586f2c663ead246170f21e8
Nabeel Nazir <nabeeln796@yahoo.com>
2017-10-20 18:00:35 +0500

    user following and followers object model with dummy implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,36 @@
 #     g.comments.create(text: Faker::Lorem.paragraphs(rand(1..2)))
 #   end
 # end
+
+# Users
+# User.create!(name:  "Example User",
+#              email: "example@railstutorial.org",
+#              password:              "foobar",
+#              password_confirmation: "foobar")
+#
+#
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   User.create!(name:  name,
+#                email: email,
+#                password: "foobar",
+#                password_confirmation: "foobar")
+#
+# end
+#
+# # Goals
+# users = User.order(:created_at).take(6)
+#
+# 50.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.goals.create!(title: content) }
+# end
+#
+# # Following relationships
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
