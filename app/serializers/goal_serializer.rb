1c749705387c5c27d9c3f3a44ede4a7c3490610b
Nabeel Nazir <nabeeln796@yahoo.com>
2017-10-24 19:24:24 +0500

    postgres added as a database and user object returned in goal

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,11 +1,16 @@
 class GoalSerializer < ActiveModel::Serializer
   belongs_to :user
   has_many :comments
-  attributes :id, :title, :email, :user_id, :created_at
+  attributes :id, :title, :email, :user_id, :created_at, :user
 
   def created_at
     new_created_at = ActionView::Base.new.distance_of_time_in_words(Time.now, object.created_at.to_time).to_s + ' ago'.titleize if object.created_at
     new_created_at
+  end
+
+  def user
+    new_user = object.user
+    new_user
   end
 
 end
